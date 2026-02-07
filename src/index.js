73bd71f7b842d93a4101b1e8a790fd1446bd0dcc
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-02-07 09:46:12 +0800

    feat: Add storageManager module

--- src/index.js
+++ src/index.js
@@ -173,4 +173,18 @@ class ChecklistItem {
   };
 }
 
+const storageManager = (() => {
+  const save = (data) => {
+    localStorage.setItem("Project List", JSON.stringify(data));
+  };
+
+  const load = () => {
+    const data = localStorage.getItem("Project List");
+    return data ? JSON.parse(data) : null;
+  };
+
+  return { save, load };
+})();
+
 window.projectController = projectController;
+window.storageManager = storageManager;
