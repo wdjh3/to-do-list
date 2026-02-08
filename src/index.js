048eb0532279e32a10c2da0391e84a85b4ce0daf
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-02-08 09:57:03 +0800

    refactor: Remove temporary storage testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { projectController } from "./controller/projectController.js";
-import { storageManager } from "./storage/storageManager.js";
 
 projectController.loadData();
 
 window.projectController = projectController;
-window.storageManager = storageManager;
