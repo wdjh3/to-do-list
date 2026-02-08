d133094834e45f0bd2120ae20f68de8e0477e3cf
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-02-08 09:36:10 +0800

    refactor: Move Task class to it's own module

---
+++ src/models/task.js
@@ -0,0 +1,73 @@
+import { ChecklistItem } from "./checklistItem.js";
+
+export class Task {
+  // TODO: move checklist to #checklist here
+  /**
+   *  @param {string} id
+   *  @param {string} title
+   *  @param {string} description
+   *  @param {Date} dueDate
+   *  @param {keyof PRIORITY} priority
+   *  @param {ChecklistItem[]} checklist
+   *  @param {boolean} isDone
+   */
+  constructor(title, description, dueDate, priority) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checklist = [];
+    this.isDone = false;
+  }
+
+  static fromJSON(data) {
+    const checklist = data.checklist.map((item) =>
+      ChecklistItem.fromJSON(item),
+    );
+    const task = new Task(
+      data.title,
+      data.description,
+      data.dueDate, // TODO: Remember to wrap with new Date() to create valid Date object for use
+      data.priority,
+    );
+
+    task.id = data.id;
+    task.checklist = checklist;
+    task.isDone = data.isDone;
+
+    return task;
+  }
+
+  edit = (title, description, dueDate, priority) => {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  };
+
+  addChecklistItem = (title, description) => {
+    this.checklist.push(new ChecklistItem(title, description));
+  };
+
+  deleteChecklistItem = (id) => {
+    if (this.getChecklistItem(id)) {
+      this.checklist = this.checklist.filter((item) => item.id !== id);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  getChecklistItems = () => {
+    return JSON.parse(JSON.stringify(this.checklist));
+  };
+
+  getChecklistItem = (id) => {
+    return this.checklist.find((item) => item.id === id);
+  };
+
+  toggleDone = () => {
+    this.isDone = !this.isDone;
+  };
+}
