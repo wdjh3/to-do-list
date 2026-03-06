eae8c6d099d142156c04e8766a70e35f70619b4a
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-06 11:31:40 +0800

    feat: Add add checklist item opening modal

--- src/controller/uiController.js
+++ src/controller/uiController.js
@@ -1,6 +1,6 @@
 const addNewProjectBtn = document.getElementById("add-new-project");
 const addNewTaskBtn = document.getElementById("add-new-task");
-const taskContainer = document.getElementById("task-container");
+const currentProject = document.getElementById("current-project");
 const projectFormDialog = document.getElementById("project-form-dialog");
 const taskFormDialog = document.getElementById("task-form-dialog");
 const checklistItemFormDialog = document.getElementById("checklist-item-form-dialog");
@@ -9,6 +9,15 @@ export const uiController = (() => {
     const start = () => {
         addNewProjectBtn.addEventListener('click', () => projectFormDialog.showModal())
         addNewTaskBtn.addEventListener('click', () => taskFormDialog.showModal())
+        currentProject.addEventListener('click', (e) => {
+            if (e.target.closest(".add-checklist-item")) {
+                checklistItemFormDialog.showModal()
+            }
+        })
+    }
+
+    const sync = () => {
+        
     }
 
     return {start}
