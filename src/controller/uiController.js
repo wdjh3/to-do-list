fce3ad716396a190e6fe0a8f55088ae524085ef6
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-07 15:25:40 +0800

    feat: Add styling to dialog elements

--- src/controller/uiController.js
+++ src/controller/uiController.js
@@ -3,22 +3,51 @@ const addNewTaskBtn = document.getElementById("add-new-task");
 const currentProject = document.getElementById("current-project");
 const projectFormDialog = document.getElementById("project-form-dialog");
 const taskFormDialog = document.getElementById("task-form-dialog");
-const checklistItemFormDialog = document.getElementById("checklist-item-form-dialog");
+const checklistItemFormDialog = document.getElementById(
+  "checklist-item-form-dialog",
+);
 
 export const uiController = (() => {
-    const start = () => {
-        addNewProjectBtn.addEventListener('click', () => projectFormDialog.showModal())
-        addNewTaskBtn.addEventListener('click', () => taskFormDialog.showModal())
-        currentProject.addEventListener('click', (e) => {
-            if (e.target.closest(".add-checklist-item")) {
-                checklistItemFormDialog.showModal()
-            }
-        })
-    }
+  const formDialogs = [
+    projectFormDialog,
+    taskFormDialog,
+    checklistItemFormDialog,
+  ];
 
-    const sync = () => {
-        
-    }
+  const start = () => {
+    addNewProjectBtn.addEventListener("click", () =>
+      projectFormDialog.showModal(),
+    );
+    addNewTaskBtn.addEventListener("click", () => taskFormDialog.showModal());
+    currentProject.addEventListener("click", (e) => {
+      if (e.target.closest(".add-checklist-item")) {
+        console.log(
+          e.target.closest(".task-container").querySelector(".task").id,
+        );
+        checklistItemFormDialog.showModal();
+      }
+    });
+    for (const formDialog of formDialogs) {
+      formDialog.addEventListener("close", () => {
+        formDialog.querySelector(".input-form").reset();
+      });
 
-    return {start}
+      formDialog.addEventListener('click', (e) => {
+        const dialogDimensions = formDialog.getBoundingClientRect();
+  
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          formDialog.close();
+        }
+      });
+    }
+  };
+
+  const sync = () => {};
+
+  return { start };
 })();
