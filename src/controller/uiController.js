c99366dca40afab9926585ca61b3669ab46335b0
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-08 13:50:54 +0800

    wip: fix item overflow

--- src/controller/uiController.js
+++ src/controller/uiController.js
@@ -18,32 +18,44 @@ export const uiController = (() => {
   const start = () => {
     addNewProjectBtn.addEventListener("click", () => {
       projectFormDialog.querySelector(".title").textContent = "Add Project";
-      projectFormDialog.querySelector("button[type='submit']").textContent = "Add Project";
+      projectFormDialog.querySelector("button[type='submit']").textContent =
+        "Add Project";
       projectFormDialog.showModal();
-      }
-    );
+    });
     addNewTaskBtn.addEventListener("click", () => {
       taskFormDialog.querySelector(".title").textContent = "Add Task";
-      taskFormDialog.querySelector("button[type='submit']").textContent = "Add Task";
-      taskFormDialog.showModal()
+      taskFormDialog.querySelector("button[type='submit']").textContent =
+        "Add Task";
+      taskFormDialog.showModal();
     });
     currentProject.addEventListener("click", (e) => {
       if (e.target.closest(".add-checklist-item")) {
-        checklistItemFormDialog.querySelector(".title").textContent = "Add Checklist Item";
-        checklistItemFormDialog.querySelector("button[type='submit']").textContent = "Add Checklist Item";
-        checklistItemFormDialog.showModal()}
+        checklistItemFormDialog.querySelector(".title").textContent =
+          "Add Checklist Item";
+        checklistItemFormDialog.querySelector(
+          "button[type='submit']",
+        ).textContent = "Add Checklist Item";
+        checklistItemFormDialog.showModal();
+      }
 
       // Handles opening the edit form dialog when the edit button is pressed for Tasks
       if (e.target.closest(".edit-button") && e.target.closest(".task")) {
         taskFormDialog.querySelector(".title").textContent = "Edit Task";
-        taskFormDialog.querySelector("button[type='submit']").textContent = "Edit Task";
+        taskFormDialog.querySelector("button[type='submit']").textContent =
+          "Edit Task";
         taskFormDialog.showModal();
       }
 
       // Handles opening the edit form dialog when the edit button is pressed for ChecklistItems
-      if (e.target.closest(".edit-button") && e.target.closest(".checklist-item")) {
-        checklistItemFormDialog.querySelector(".title").textContent = "Edit Checklist Item";
-        checklistItemFormDialog.querySelector("button[type='submit']").textContent = "Edit Checklist Item";
+      if (
+        e.target.closest(".edit-button") &&
+        e.target.closest(".checklist-item")
+      ) {
+        checklistItemFormDialog.querySelector(".title").textContent =
+          "Edit Checklist Item";
+        checklistItemFormDialog.querySelector(
+          "button[type='submit']",
+        ).textContent = "Edit Checklist Item";
         checklistItemFormDialog.showModal();
       }
     });
@@ -51,10 +63,11 @@ export const uiController = (() => {
     projectList.addEventListener("click", (e) => {
       if (e.target.closest(".edit-button")) {
         projectFormDialog.querySelector(".title").textContent = "Edit Project";
-        projectFormDialog.querySelector("button[type='submit']").textContent = "Edit Project";
+        projectFormDialog.querySelector("button[type='submit']").textContent =
+          "Edit Project";
         projectFormDialog.showModal();
       }
-    })
+    });
 
     for (const formDialog of formDialogs) {
       formDialog.addEventListener("close", () => {
@@ -62,9 +75,9 @@ export const uiController = (() => {
       });
 
       // Close form if click outside dialog window
-      formDialog.addEventListener('click', (e) => {
+      formDialog.addEventListener("click", (e) => {
         const dialogDimensions = formDialog.getBoundingClientRect();
-  
+
         if (
           e.clientX < dialogDimensions.left ||
           e.clientX > dialogDimensions.right ||
